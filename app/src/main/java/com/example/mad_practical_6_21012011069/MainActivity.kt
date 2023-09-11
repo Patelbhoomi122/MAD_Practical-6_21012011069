5bfe10ddcf8576209f97c4b5a3e567f70477f65a
student <student@gnu.ac.in>
2023-09-11 09:00:21 +0530

    practical_6

--- app/src/main/java/com/example/mad_practical_6_21012011069/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_6_21012011069/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.mad_practical_6_21012011069
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val start= findViewById<Button>(R.id.play_button)
+        start.setOnClickListener {
+            Intent(applicationContext,MyService::class.java).putExtra(MyService.playerconstant,"fhg").apply {
+                startService(this)
+            }
+        }
+        val stop=findViewById<Button>(R.id.stop_button)
+        stop.setOnClickListener {
+            Intent(applicationContext,MyService::class.java).putExtra(MyService.playerconstant,"fhg").apply {
+                stopService(this)
+            }
+        }
+
     }
-}
+    }
+
